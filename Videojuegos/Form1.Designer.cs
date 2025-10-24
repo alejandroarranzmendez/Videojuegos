68d24b8c48643561a9a42f1a454bb190f05a437e
Campas10 <arranzmendezalejandro@gmail.com>
2025-10-24 19:11:48 +0200

    Agregar archivos de proyecto.

---
+++ Videojuegos/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Videojuegos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1 = new GroupBox();
+            button12 = new Button();
+            button11 = new Button();
+            button8 = new Button();
+            button10 = new Button();
+            groupBox2 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(224, 224, 224);
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(64, 64, 64);
+            button1.Location = new Point(25, 25);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 50);
+            button1.TabIndex = 0;
+            button1.Text = "Destacados";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(224, 224, 224);
+            button2.Cursor = Cursors.Hand;
+            button2.Font = new Font("Impact", 12F);
+            button2.ForeColor = Color.FromArgb(64, 64, 64);
+            button2.Location = new Point(25, 100);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 50);
+            button2.TabIndex = 1;
+            button2.Text = "Mis juegos";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(224, 224, 224);
+            button3.Cursor = Cursors.Hand;
+            button3.Font = new Font("Impact", 12F);
+            button3.ForeColor = Color.FromArgb(64, 64, 64);
+            button3.Location = new Point(25, 175);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 50);
+            button3.TabIndex = 2;
+            button3.Text = "Configuración";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(224, 224, 224);
+            button4.Cursor = Cursors.Hand;
+            button4.Font = new Font("Impact", 12F);
+            button4.ForeColor = Color.FromArgb(64, 64, 64);
+            button4.Location = new Point(25, 250);
+            button4.Name = "button4";
+            button4.Size = new Size(150, 50);
+            button4.TabIndex = 3;
+            button4.Text = "¿?";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Silver;
+            groupBox1.Controls.Add(button12);
+            groupBox1.Controls.Add(button11);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button10);
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1920, 100);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(224, 224, 224);
+            button12.Cursor = Cursors.Hand;
+            button12.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.ForeColor = Color.FromArgb(64, 64, 64);
+            button12.Location = new Point(1745, 25);
+            button12.Name = "button12";
+            button12.Size = new Size(150, 50);
+            button12.TabIndex = 4;
+            button12.Text = "Mi Perfil";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.FromArgb(224, 224, 224);
+            button11.Cursor = Cursors.Hand;
+            button11.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.ForeColor = Color.FromArgb(64, 64, 64);
+            button11.Location = new Point(960, 25);
+            button11.Name = "button11";
+            button11.Size = new Size(150, 50);
+            button11.TabIndex = 3;
+            button11.Text = "Soporte";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(224, 224, 224);
+            button8.Cursor = Cursors.Hand;
+            button8.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.ForeColor = Color.FromArgb(64, 64, 64);
+            button8.Location = new Point(785, 25);
+            button8.Name = "button8";
+            button8.Size = new Size(150, 50);
+            button8.TabIndex = 2;
+            button8.Text = "Comunidad";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(224, 224, 224);
+            button10.Cursor = Cursors.Hand;
+            button10.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.ForeColor = Color.FromArgb(64, 64, 64);
+            button10.Location = new Point(610, 25);
+            button10.Name = "button10";
+            button10.Size = new Size(150, 50);
+            button10.TabIndex = 1;
+            button10.Text = "Tienda";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.Silver;
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Location = new Point(0, 150);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 325);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(250, 150);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(225, 300);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.InitialImage = null;
+            pictureBox2.Location = new Point(525, 150);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(225, 300);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.InitialImage = null;
+            pictureBox3.Location = new Point(800, 150);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(225, 300);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.InitialImage = null;
+            pictureBox4.Location = new Point(1075, 150);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(225, 300);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.InitialImage = null;
+            pictureBox5.Location = new Point(1350, 150);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(225, 300);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 10;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.InitialImage = null;
+            pictureBox6.Location = new Point(1625, 150);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(225, 300);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 11;
+            pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "ArranzGames";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private GroupBox groupBox1;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button10;
+        private Button button9;
+        private Button button12;
+        private Button button11;
+        private Button button8;
+        private GroupBox groupBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+    }
+}
---
+++ Videojuegos/Form1.cs
@@ -0,0 +1,59 @@
+namespace Videojuegos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
